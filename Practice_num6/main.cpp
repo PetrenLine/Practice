0d087236ed48746f22a868c2b8949bb4a3ca9bc5
PetrLine <aweryn852@mail.ru>
2023-07-31 12:00:13 +0300

    6.113

--- Practice_num6/main.cpp
+++ Practice_num6/main.cpp
@@ -1,15 +1,17 @@
 #include <QCoreApplication>
 #include<iostream>
 #include <cmath>
+#include<string.h>
 
 int main()
 {
+
   {
     //Task 6.10
     int n;
     int i = 1;
 
-    std::cout << "Enter the number" << std::endl;
+    std::cout << "6.10" << std::endl;
     std::cout << "Entered number:";
     std::cin >> n;
     std::cout << "a)" << std::endl;
@@ -26,11 +28,34 @@ int main()
         //b
         while ( pow( i, 2 ) <= n + 1 )
         {
-          std::cout << "b)" << std::endl;
+          std::cout << "b) " << std::endl;
           std::cout << "  " << i << std::endl;
           i++;
         }
       }
     }
+  }
+  {
+
+    //Task 6.113
+    std::cout << "6.113" << std::endl;
+    int n;
+    int m;
+    int count = 0;
+    int sum = 0;
+    std:: cout << "Enter the number n: ";
+    std:: cin >> n;
+    std:: cout << "Enter the number m: ";
+    std:: cin >> m;
+
+    while ( count < m )
+    {
+      int d = n % 10;
+      sum += d;
+      n /= 10;
+      count++;
+    }
+
+    std::cout << "Summa " << m << " Last numbers = " << sum << std::endl;
   }
 }
