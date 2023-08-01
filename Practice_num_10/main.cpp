5d34bb7ec9dd0eea47a8939ecc9a305b122dd793
PetrLine <aweryn852@mail.ru>
2023-08-01 19:23:01 +0300

    Task 10.54

--- Practice_num_10/main.cpp
+++ Practice_num_10/main.cpp
@@ -1,16 +1,35 @@
 #include <QCoreApplication>
 #include <iostream>
+#include <string>
 
 void Grafic()
 {
   for ( int i = 0; i < 80; ++i )
     std::cout << "*";
 }
+void dec( const int num, std::string& snum )
+{
+  if ( num % 2 == 0 )
+    snum += "0";
+  else snum += "1";
 
+  if ( ( num / 2 ) != 0 ) dec( num / 2, snum );
+  else return;
+
+  reverse( snum.begin(), snum.end() );
+}
 int main()
 {
-  std::cout << "Task 10.33" << std::endl;
-  Grafic();
-  std::cout << "" << std::endl;
-  return 0;
+  {
+    std::cout << "Task 10.33" << std::endl;
+    Grafic();
+    std::cout << "" << std::endl;
+
+    {
+      std::cout << "Task 10.54" << std::endl;
+      std::string snum;
+      dec( 60, snum );
+      std:: cout << "The value of this number inbinary system: " << snum << std::endl;
+    }
+  }
 }
